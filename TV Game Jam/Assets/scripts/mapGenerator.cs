d30978f23474ca6f79bc63d2fa65ff2ece13aea4
Aidan-B <31750217+Aidan-B@users.noreply.github.com>
2019-02-05 16:59:38 -0500

    Map algorithm updates

--- TV Game Jam/Assets/scripts/mapGenerator.cs
+++ TV Game Jam/Assets/scripts/mapGenerator.cs
@@ -6,18 +6,20 @@ public class mapGenerator : MonoBehaviour
 {
     public GameObject square;
 
-    public const int Width = 50;
-    public const int Height = 50;
-    public const int WalkerPaths = 150;
-    
+    public const int Width = 500;
+    public const int Height = 500;
+    public const int WalkerPaths = 500;
+    [Range(0, 1)]public float xBias = 0.5f;
+    [Range(0, 1)] public float yBias = 0.25f;
+
+
     private mapTile[,] map = new mapTile[Width, Height];
     private bool[,] mapPaths = new bool[Width, Height];
     private Vector2[,] directions = new Vector2[2,2] { { Vector2.up, Vector2.down }, { Vector2.left, Vector2.right } };
 
     // Start is called before the first frame update
     void Start()
-    {  
-        
+    {
         Vector2 startPos = new Vector2(Random.Range(0, Width), Random.Range(0, Height));
         Vector2 currentPos = startPos;
         Vector2 currentDir = directions[Random.Range(0, 1), Random.Range(0, 1)]; //random direction
@@ -38,7 +40,7 @@ public class mapGenerator : MonoBehaviour
             {
                 if (currentPos.y <= 1)
                     currentDir = Vector2.up;
-                else if (currentPos.x >= Height - 1)
+                else if (currentPos.y >= Height - 1)
                     currentDir = Vector2.down;
                 else
                     currentDir = directions[0, Random.Range(0, 2)]; // up/down
@@ -47,30 +49,31 @@ public class mapGenerator : MonoBehaviour
 
             if (currentDir == Vector2.up)
             {
-                distance = Random.Range(0, Height - (int)currentPos.y);
+                distance = Random.Range(1, (int)((Height - currentPos.y)  * yBias));
             }
             else if (currentDir == Vector2.down)
             {
-                distance = Random.Range(0, (int)currentPos.y);
+                distance = Random.Range(1, (int)(currentPos.y * yBias));
             }
             else if (currentDir == Vector2.left)
             {
-                distance = Random.Range(0, (int)currentPos.x);
+                distance = Random.Range(1, (int)(currentPos.x * xBias));
             }
             else if (currentDir == Vector2.right)
             {
-                distance = Random.Range(0, Width - (int)currentPos.x);
+                distance = Random.Range(1, (int)((Width - currentPos.x) * xBias));
             }
             else
             {
                 Debug.Log("Error");
             }
-
+            //Debug.Log("Distance: " + distance + currentDir);
             for (int step = 0; step < distance; step++)
             {
+                currentPos += currentDir;
+                //Debug.Log("Position: " + currentPos);
                 mapPaths[(int)currentPos.x, (int)currentPos.y] = true;
                 //Debug.Log("Current Position: " + currentPos.x + ", " + currentPos.y);
-                currentPos += currentDir;
             }
         }
 
