24382b305603a1c368b28f528a2e4013ad6a8cf6
Aidan-B <31750217+Aidan-B@users.noreply.github.com>
2019-02-05 18:18:49 -0500

    Map generation biases and tweaking

--- TV Game Jam/Assets/scripts/mapGenerator.cs
+++ TV Game Jam/Assets/scripts/mapGenerator.cs
@@ -8,9 +8,11 @@ public class mapGenerator : MonoBehaviour
 
     public const int Width = 500;
     public const int Height = 500;
-    public const int WalkerPaths = 500;
-    [Range(0, 1)]public float xBias = 0.5f;
-    [Range(0, 1)] public float yBias = 0.25f;
+    public int WalkerPaths = 2000;
+    [Range(0, 1)]public float xBias = 0.1f; //longer horizontal tunnels
+    [Range(0, 1)] public float yBias = 0.01f; //longer vertical tunnels
+    [Range(0.5f, 2f)] public float RightLeftBias = 2f; // >1 is right, <1 is left
+    [Range(0.5f, 2f)] public float DownUpBias = 1.5f; // >1 is down, <1 is up
 
 
     private mapTile[,] map = new mapTile[Width, Height];
@@ -20,10 +22,12 @@ public class mapGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Vector2 startPos = new Vector2(Random.Range(0, Width), Random.Range(0, Height));
+        Vector2 startPos = new Vector2(Mathf.RoundToInt(Width * 0.5f), Mathf.RoundToInt(Height * 0.5f));
         Vector2 currentPos = startPos;
         Vector2 currentDir = directions[Random.Range(0, 1), Random.Range(0, 1)]; //random direction
         int distance = 0;
+        int roomCounter = 0;
+        mapPaths[(int)currentPos.x, (int)currentPos.y] = true;
         for (int moves = 0; moves < WalkerPaths; moves++)
         {
             //Change Direction, move away from map bounds if near it
@@ -34,7 +38,7 @@ public class mapGenerator : MonoBehaviour
                 else if (currentPos.x >= Width - 1)
                     currentDir = Vector2.left;
                 else
-                    currentDir = directions[1, Random.Range(0, 2)];// left/right
+                    currentDir = directions[1, Mathf.RoundToInt(Mathf.Clamp( Random.Range(0f, 1f) * RightLeftBias, 0f, 1f))];// left/right
             }
             else
             {
@@ -43,7 +47,7 @@ public class mapGenerator : MonoBehaviour
                 else if (currentPos.y >= Height - 1)
                     currentDir = Vector2.down;
                 else
-                    currentDir = directions[0, Random.Range(0, 2)]; // up/down
+                    currentDir = directions[0, Mathf.RoundToInt(Mathf.Clamp(Random.Range(0f, 1f) * DownUpBias, 0f, 1f))]; // up/down
             }
             
 
@@ -74,6 +78,7 @@ public class mapGenerator : MonoBehaviour
                 //Debug.Log("Position: " + currentPos);
                 mapPaths[(int)currentPos.x, (int)currentPos.y] = true;
                 //Debug.Log("Current Position: " + currentPos.x + ", " + currentPos.y);
+                
             }
         }
 
@@ -84,13 +89,14 @@ public class mapGenerator : MonoBehaviour
                 if (mapPaths[x,y])
                 {
                     if (startPos == new Vector2(x,y))
-                        Instantiate(square, new Vector3(x, y, 1), Quaternion.identity, gameObject.GetComponent<Transform>());
+                        Instantiate(square, new Vector3(x, y, 10), Quaternion.identity, gameObject.GetComponent<Transform>());
                     else
                         Instantiate(square, new Vector3(x, y), Quaternion.identity, gameObject.GetComponent<Transform>());
+                    roomCounter++;
                 }
             }
         }
-        
+        Debug.Log(roomCounter);
     }
 
     // Update is called once per frame
