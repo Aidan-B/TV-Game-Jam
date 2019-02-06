888cb7d419c57a003b1f76ae1f5d7059ecb9fae8
Aidan-B <31750217+Aidan-B@users.noreply.github.com>
2019-02-06 13:52:57 -0500

    Tile Mapping

---
+++ TV Game Jam/Assets/scripts/randomTexture.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class randomTexture : MonoBehaviour
+{
+    public List<Sprite> sprites;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Count)];
+    }
+}
