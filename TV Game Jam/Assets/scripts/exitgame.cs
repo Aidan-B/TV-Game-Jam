52c01a36405fccfa4a088bb9c8fd66f2c990f1f3
silkyrie <32624451+silkyrie@users.noreply.github.com>
2019-02-15 00:46:52 -0500

    exit game button works now

---
+++ TV Game Jam/Assets/scripts/exitgame.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitgame : MonoBehaviour {
+    public void quitgame() {
+        Application.Quit();
+    }
+}
